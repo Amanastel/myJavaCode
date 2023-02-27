314e11184b925c460a74da3ba40be5d1959c14ed
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-02-27 15:53:29 +0530

    java

--- javaMasai/OOPS/OOPS/src/anuj/com/CollectionFramework.java
+++ javaMasai/OOPS/OOPS/src/anuj/com/CollectionFramework.java
@@ -4,5 +4,8 @@ package anuj.com;
 public class CollectionFramework {
     public static void main(String[] args) {
         System.out.println("hello");
+        for(int i=0; i<6; i++){
+            System.out.println("hello world");
+        }
     }
 }
