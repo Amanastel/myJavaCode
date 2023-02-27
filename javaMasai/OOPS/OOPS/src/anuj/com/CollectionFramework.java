cc87373559137f295ad3bfe38ffdaf60f6d17c44
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-02-27 15:55:09 +0530

    javat

--- javaMasai/OOPS/OOPS/src/anuj/com/CollectionFramework.java
+++ javaMasai/OOPS/OOPS/src/anuj/com/CollectionFramework.java
@@ -5,7 +5,12 @@ public class CollectionFramework {
     public static void main(String[] args) {
         System.out.println("hello");
         for(int i=0; i<6; i++){
-            System.out.println("hello world");
+//            System.out.println("hello world");
+        }
+        int i = 0;
+        while (i<6){
+            System.out.println(i);
+            i++;
         }
     }
 }
