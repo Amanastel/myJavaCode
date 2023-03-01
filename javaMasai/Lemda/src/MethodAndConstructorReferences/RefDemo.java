49590c01d07c1e782a99b5de615327a440df6ea6
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-02 02:54:44 +0530

    refer

--- javaMasai/Lemda/src/MethodAndConstructorReferences/RefDemo.java
+++ javaMasai/Lemda/src/MethodAndConstructorReferences/RefDemo.java
@@ -9,7 +9,29 @@ public class RefDemo {
 //        WorkInter workInter=()->{
 //            System.out.println("this is do task new method ");
 //        };
+
+        // static method ko refer kiya hai
+        // className::methodName
         WorkInter workInter = Stuff::doSuff;
         workInter.doTask();  // this line of code refer the all code where i write doStuff static
+
+
+        Runnable runnable = Stuff::threadTask;
+
+
+        WorkInter workInter1 = Stuff::threadTask;
+        workInter1.doTask();
+
+
+        Thread d = new Thread(runnable);
+        d.start();
+
+
+        Stuff ob = new Stuff();
+        Runnable runnable1 = ob::printNumber;
+        Thread d2 = new Thread(runnable1);
+        d2.start();
+//        runnable1.run();
+
     }
 }
--- javaMasai/Lemda/src/MethodAndConstructorReferences/com/Stuff.java
+++ javaMasai/Lemda/src/MethodAndConstructorReferences/com/Stuff.java
@@ -13,4 +13,33 @@ public class Stuff {
         System.out.println(now);
     }
 
+
+    public static void threadTask(){
+        int n = 10;
+        for(int i=1; i<=n; i++){
+            System.out.println(i*2);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // this is a none static method we can not call directly
+    public void printNumber(){
+        int n = 10;
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
