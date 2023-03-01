77f3faf5afdcfeb8a7d24240a93a9eceee394b7e
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-02 02:06:35 +0530

    jkkj

--- javaMasai/Lemda/src/Main.java
+++ javaMasai/Lemda/src/Main.java
@@ -12,6 +12,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         for(int i=0; i<5; i++){
             System.out.println("hello world! "+ i);
         }
