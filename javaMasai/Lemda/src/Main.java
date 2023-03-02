c6a4a0f069801f78ba3d5dca31e7e50edd978b05
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-03 00:25:15 +0530

    l

--- javaMasai/Lemda/src/Main.java
+++ javaMasai/Lemda/src/Main.java
@@ -65,6 +65,10 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
     }
 }
 
