c2505717afda77aeb1064efd554feb3af1ea90d9
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-03 02:05:15 +0530

    stream api 01

---
+++ javaMasai/Lemda/src/StreamAPI/StreamMain.java
@@ -0,0 +1,28 @@
+package StreamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        // Create a List and Filter all even Number form list
+
+//        List<Integer> list1 = new java.util.ArrayList<>(List.of(2, 3, 50, 12, 23, 43, 434, 55));
+//        list1.add(34);
+
+        List<Integer> list1 = List.of(2,3,50,12,23,43,434,55);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(14);
+        list2.add(32);
+        list2.add(43);
+        list2.add(21);
+        list2.add(12);
+
+        List<Integer> list3 = Arrays.asList(23,567,12,677,24);
+        System.out.println(list2);
+    }
+}
