7faa8f58a20e44882d26e7f511adf1b32b853cbb
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-04 02:41:26 +0530

    cmplt Generic

--- javaMasai/Generic/src/Generic/Box.java
+++ javaMasai/Generic/src/Generic/Box.java
@@ -1,11 +1,17 @@
 package Generic;
 
-public class Box {
-    Object container;
-    public Box(Object container){
+public class Box<T> {
+    T container;
+    public Box(T container){
         this.container = container;
     }
-
+    public void perFormSomeTask(){
+        if(container instanceof String){
+            System.out.println("length of "+ container+ " is " + (((String)this.container).length()));
+        } else if (container instanceof Integer) {
+            System.out.println("this is a integer value "+ container);
+        }
+    }
     public Object getValue(){
         return this.container;
     }
--- javaMasai/Generic/src/Generic/Example.java
+++ javaMasai/Generic/src/Generic/Example.java
@@ -2,9 +2,20 @@ package Generic;
 
 public class Example {
     public static void main(String[] args) {
-        Box box = new Box("water");
+        /*Box box = new Box("water");
         Box box1 = new Box(12);
         System.out.println(box.getValue());
+        System.out.println(box1.getValue());*/
+
+        Box<String> box = new Box<String>("Wow this is amazing");
+        System.out.println(box.getValue());
+        System.out.println(box.container.getClass().getName());
+
+        Box<Integer> box1 = new Box<>(122);
         System.out.println(box1.getValue());
+        System.out.println(box1.container.getClass().getName());
+
+        box.perFormSomeTask();
+        box1.perFormSomeTask();
     }
 }
