e1f1c1f0d5438530fa46532faf0e69242b2efce1
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-03 18:13:21 +0530

    javaka

--- javaMasai/Lemda/src/StreamAPI/StreamMain.java
+++ javaMasai/Lemda/src/StreamAPI/StreamMain.java
@@ -3,10 +3,15 @@ package StreamAPI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("hello");
+//        System.out.println("hello");
 
         // Create a List and Filter all even Number form list
 
@@ -24,15 +29,49 @@ public class StreamMain {
 
         List<Integer> list3 = Arrays.asList(23,567,12,677,24);
 
+        Stream<Integer> data = list3.stream(); // we have to use stream once time only
+//        data.forEach(i-> System.out.println(i));
+
+        Stream<Integer> sortData = data.sorted(); // we have to use stream once time only
+//        sortData.forEach(i-> System.out.println(i));
+        //data.forEach(i-> System.out.println(i));  we have to use stream once time only
+
+
+
         List<Integer> listEven = new ArrayList<>();
 
-        System.out.println(listEven);
+//        System.out.println(listEven);
         for(Integer i: list1){
             if(i%2==0){
                 listEven.add(i);
             }
         }
-        System.out.println(list1);
-        System.out.println(listEven);
+
+        Stream<Integer> stream = list1.stream();
+//        System.out.println(stream);
+        List<Integer> collecct = stream.filter(i->i%2==0).collect(Collectors.toList());
+//        System.out.println(collecct);
+
+
+        List<Integer> listNumbers = Arrays.asList(10, 10, 20, 50, 78, 96, 14, 35);
+
+        listNumbers.stream().distinct().forEach(i-> System.out.print(i + " "));
+        System.out.println();
+        listNumbers.stream().filter(i -> i % 2 == 0).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+        listNumbers.stream().distinct().filter(i -> i % 2 == 0).forEach(i -> System.out.print(i + " "));
+
+        List<String> nameList = Arrays.asList("abc","bcv","kks","aman","astel");
+//        System.out.println();
+        System.out.println();
+        Function<String,Integer> fn = name -> name.length();
+//        List<Integer> listLength = nameList.stream().map(fn).toList();
+        List<Integer> listLength = nameList.stream().filter(name -> name.length()>3).map(fn).toList();
+        listLength.forEach(i -> System.out.print (i + " "));
+
+        
+        System.out.println();
+        nameList.stream().sorted().forEach(i -> System.out.println(i));
+
     }
 }
