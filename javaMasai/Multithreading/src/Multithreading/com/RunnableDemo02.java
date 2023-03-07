1f1c08839920b36db644649bd768d7607979bd85
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-07 07:22:08 +0530

    thread

--- javaMasai/Multithreading/src/Multithreading/com/RunnableDemo02.java
+++ javaMasai/Multithreading/src/Multithreading/com/RunnableDemo02.java
@@ -16,6 +16,7 @@ class MyThreadRunnable implements Runnable{
         System.out.println("I am a Thread 1 not a thread1");
         System.out.println("I am a Thread 1 not a thread1");
         System.out.println("I am a Thread 1 not a thread1");
+
     }
 }
 
---
+++ javaMasai/Multithreading/src/Multithreading/com/ThreadConstructor.java
@@ -0,0 +1,30 @@
+package Multithreading.com;
+
+
+class MyThr extends Thread{
+    public MyThr(String name){
+        super(name);
+    }
+
+    public void run(){
+        int i = 34;
+        System.out.println("Thank you");
+//        while (true){
+//            System.out.println("I am a thread");
+//        }
+    }
+
+}
+public class ThreadConstructor {
+    public static void main(String[] args) {
+        MyThr thr = new MyThr("Aman");
+        MyThr thr2 = new MyThr("anshika");
+        thr.start();
+        thr2.start();
+        System.out.println("the id is the thread " + thr.getId());
+        System.out.println("the id is the thread " + thr.getName());
+
+        System.out.println("the id is the thread " + thr2.getId());
+        System.out.println("the id is the thread " + thr2.getName());
+    }
+}
