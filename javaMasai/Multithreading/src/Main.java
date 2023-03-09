a6f5b83e3d0f0b82360022b2609a43a7b703abf8
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-09 11:04:17 +0530

    bjb

--- javaMasai/Multithreading/src/Main.java
+++ javaMasai/Multithreading/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("hello world");
     }
 }
