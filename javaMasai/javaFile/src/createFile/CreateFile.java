715f303496a6fa09a5b3eb8990b2ba85519f5fe3
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-14 12:46:36 +0530

    worte

--- javaMasai/javaFile/src/createFile/CreateFile.java
+++ javaMasai/javaFile/src/createFile/CreateFile.java
@@ -1,15 +1,17 @@
 package createFile;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CreateFile {
     public static void main(String[] args) {
         File f = new File("A.text");
-
+//        FileWriter
         try {
             if(f.createNewFile()){
                 System.out.println("file Successfully created..!");
+
             }else {
                 System.out.println("file already exist..!");
             }
---
+++ javaMasai/javaFile/src/createFile/FileWri.java
@@ -0,0 +1,30 @@
+package createFile;
+
+//import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWri {
+    public static void main(String[] args) throws IOException{
+//        FileWriter a = new FileWriter("");
+//        try {
+//            FileWriter f = new FileWriter("we.test");
+//            try {
+//                f.write("Java Programming is tge best Language..!");
+//            }finally {
+//                f.close();
+//            }
+//            System.out.println("Successfully Data wrote in file..!");
+//        }catch (IOException e){
+//            System.out.println(e);
+////            e.printStackTrace();
+//        }
+
+
+
+        // if we can handhold Exception
+        FileWriter f = new FileWriter("we.test");
+        f.write("Java Programming is tge best Language..!");
+        System.out.println("Successfully Data wrote in file..!");
+    }
+}
