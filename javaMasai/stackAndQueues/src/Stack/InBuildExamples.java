698d3a5dd73428156be8cace269f96d7dbf364dc
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-17 20:25:55 +0530

    dsa stack and queue:

---
+++ javaMasai/stackAndQueues/src/Stack/InBuildExamples.java
@@ -0,0 +1,40 @@
+package Stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class InBuildExamples {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+        queue.add(19);
+        queue.add(1);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+
+
+    }
+}
