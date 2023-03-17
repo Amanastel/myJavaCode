b14a0f283f676cb3632f76973016655884f52360
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-03-18 01:39:48 +0530

    dsa stack

--- javaMasai/stackAndQueues/src/Stack/InBuildExamples.java
+++ javaMasai/stackAndQueues/src/Stack/InBuildExamples.java
@@ -1,40 +1,44 @@
 package Stack;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class InBuildExamples {
     public static void main(String[] args) {
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(34);
-        stack.push(45);
-        stack.push(2);
-        stack.push(9);
-        stack.push(18);
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(34);
+//        stack.push(45);
+//        stack.push(2);
+//        stack.push(9);
+//        stack.push(18);
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//
+//
+//        Queue<Integer> queue = new LinkedList<>();
+//        queue.add(3);
+//        queue.add(6);
+//        queue.add(5);
+//        queue.add(19);
+//        queue.add(1);
+//
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
 
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(3);
-        queue.add(6);
-        queue.add(5);
-        queue.add(19);
-        queue.add(1);
-
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-
-
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(89);
+        deque.add(78);
+        deque.add(8);
+        System.out.println(deque);
+        deque.addLast(26);
 
     }
 }
