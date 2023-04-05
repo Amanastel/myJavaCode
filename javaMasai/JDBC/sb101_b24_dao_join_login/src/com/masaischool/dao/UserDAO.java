bf2e4f626ac9e9d8016c144647c0d37fb5ab366b
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-05 21:06:46 +0530

    m

--- javaMasai/JDBC/sb101_b24_dao_join_login/src/com/masaischool/dao/UserDAO.java
+++ javaMasai/JDBC/sb101_b24_dao_join_login/src/com/masaischool/dao/UserDAO.java
@@ -4,7 +4,9 @@ import com.masaischool.exception.NoRecordFoundException;
 import com.masaischool.exception.SomethingWentWrongException;
 
 public interface UserDAO {
+
 	public void login(String username, String password) 
 			throws SomethingWentWrongException, NoRecordFoundException;
+			
 	public void logout();
 }
