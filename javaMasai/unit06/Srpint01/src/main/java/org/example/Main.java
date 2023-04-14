79f1a663476abe6c0eca013b063eb0ff3dc11f30
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-15 04:10:29 +0530

    r

--- javaMasai/unit06/Srpint01/src/main/java/org/example/Main.java
+++ javaMasai/unit06/Srpint01/src/main/java/org/example/Main.java
@@ -6,16 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            System.out.println(i);
-            List<Integer> integers = new ArrayList<>();
-            integers.add(1);
-            integers.add(2);
-            integers.add(3);
-            for (Integer integer : integers) {
-                System.out.println(integer);
-            }
-        }
+
     }
 }
