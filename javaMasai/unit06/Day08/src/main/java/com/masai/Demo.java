9e01a448ccb79b87d158c3cc11e639d1fcdc204f
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-22 04:17:21 +0530

    cmplt xml

--- javaMasai/unit06/Day08/src/main/java/com/masai/Demo.java
+++ javaMasai/unit06/Day08/src/main/java/com/masai/Demo.java
@@ -6,12 +6,33 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.sql.SQLException;
 
 public class Demo {
+
+//    public Demo(){
+//
+//    }
+
+//    @Override
+//    protected void finalize() throws Throwable {
+//        System.out.println("this is a garbage collector");
+//    }
+
     public static void main(String[] args) throws SQLException {
+
+        /*Demo demo = new Demo();
+        demo = null;  garbage collector finalize()
+        System.gc();*/
+
         ApplicationContext ctx= new ClassPathXmlApplicationContext("applicationContext.xml");
+
         A obj = ctx.getBean("a1", A.class);
+        A obj2 = ctx.getBean("a1", A.class);
+        System.out.println(obj2 == obj);
 
         obj.insertStudentRecord();
+
+
         ClassPathXmlApplicationContext ctx2 = (ClassPathXmlApplicationContext) ctx;
         ctx2.close();
+
     }
 }
