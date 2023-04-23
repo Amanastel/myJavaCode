7f72f39ce7fea408be1675c88b2ac450a8cd05c6
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-24 03:02:13 +0530

    init and distory

--- javaMasai/unit06/annotationBased/src/main/java/com/masai/A.java
+++ javaMasai/unit06/annotationBased/src/main/java/com/masai/A.java
@@ -3,6 +3,9 @@ package com.masai;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service(value = "a1")
 public class A {
 
@@ -14,9 +17,22 @@ public class A {
         this.b1 = b1;
     }
 
+    @PostConstruct
+    public void setUp() {
+        System.out.println("inside set up method");
+    }
+
+    @PreDestroy
+    public void tearDown(){
+        System.out.println("inside tear down method");
+    }
+
+
     public void funA() {
         System.out.println("inside funA of A");
         System.out.println("B class object :"+ b1);
     }
+
+
 
 }
