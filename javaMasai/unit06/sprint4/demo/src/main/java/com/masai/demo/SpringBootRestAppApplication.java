80be0400b5b35fe87e6a8537c0279864f9cd0de1
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-26 03:05:18 +0530

    first sprint boot rest api project

---
+++ javaMasai/unit06/sprint4/demo/src/main/java/com/masai/demo/SpringBootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.masai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestAppApplication.class, args);
+	}
+
+}
