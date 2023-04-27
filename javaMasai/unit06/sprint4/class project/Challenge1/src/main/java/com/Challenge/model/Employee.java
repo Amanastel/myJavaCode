30f3402a140e906866b3932de2ad80e542725156
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-04-28 02:09:34 +0530

    challenge

---
+++ javaMasai/unit06/sprint4/class project/Challenge1/src/main/java/com/Challenge/model/Employee.java
@@ -0,0 +1,9 @@
+package com.Challenge.model;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String position;
+}
