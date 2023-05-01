62473c96ac737043b841524cb4e2916b87630304
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-05-02 05:04:58 +0530

    swagger

--- javaMasai/unit06/sprint4/SpringbootrelationshipappApplication/src/main/java/com/masai/controller/CourseController.java
+++ javaMasai/unit06/sprint4/SpringbootrelationshipappApplication/src/main/java/com/masai/controller/CourseController.java
@@ -5,11 +5,13 @@ import com.masai.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CourseController {
 
     @Autowired
--- javaMasai/unit06/sprint4/SpringbootrelationshipappApplication/src/main/java/com/masai/controller/StudentController.java
+++ javaMasai/unit06/sprint4/SpringbootrelationshipappApplication/src/main/java/com/masai/controller/StudentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
