c7bac82e4b1b3c85441f73fec977c3da277c1a76
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-05-02 04:38:48 +0530

    ratan sir code

--- javaMasai/unit06/sprint4/ratan sir/springbootrelationshipapp/src/main/java/com/masai/SpringbootrelationshipappApplication.java
+++ javaMasai/unit06/sprint4/ratan sir/springbootrelationshipapp/src/main/java/com/masai/SpringbootrelationshipappApplication.java
@@ -3,12 +3,14 @@ package com.masai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-public class DemoApplication {
+@EnableSwagger2
+public class SpringbootrelationshipappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
+		SpringApplication.run(SpringbootrelationshipappApplication.class, args);
 	}
 
 }
