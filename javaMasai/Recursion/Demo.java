8a4496874f67218a0d8a3045e8597a1dbac84532
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-05-16 21:13:24 +0530

    kk

---
+++ javaMasai/Recursion/Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+	public static void main(String[] args){
+
+		for(int i=1; i<11; i++){
+			System.out.println(i);
+			System.out.println("hekoo");
+			System.out.println(i);
+		}
+
+		
+
+	}
+}
