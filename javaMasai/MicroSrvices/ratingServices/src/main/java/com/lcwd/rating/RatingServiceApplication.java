b18ca23e0020204b6162f9fe1e633aa5aa96a1a4
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-05-31 06:40:38 +0530

    rating srevice cmplt

---
+++ javaMasai/MicroSrvices/ratingServices/src/main/java/com/lcwd/rating/RatingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.lcwd.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+    }
+
+}
