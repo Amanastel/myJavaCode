b610afe8c83d03c695ccbc982dc1855a1f3f56d0
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-02 05:18:49 +0530

    retry service

--- javaMasai/unit06/MicroSrvices/User/src/main/java/com/lcwd/user/service/Service/Controller/UserController.java
+++ javaMasai/unit06/MicroSrvices/User/src/main/java/com/lcwd/user/service/Service/Controller/UserController.java
@@ -3,14 +3,19 @@ package com.lcwd.user.service.Service.Controller;
 
 import com.lcwd.user.service.Service.entities.User;
 import com.lcwd.user.service.Service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
+@Slf4j
 @RequestMapping("/users")
 public class UserController {
 
@@ -24,10 +29,31 @@ public class UserController {
     }
 
 
+    int retryCount = 0;
     @GetMapping("/{userId}")
+//    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserByIdHandler(@PathVariable String userId){
+        log.info("Retry count {}: ",retryCount);
+        retryCount++;
         return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
     }
+
+
+    //creating fall back method for circuitBreaker
+
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+//        log.info("Fallback is executed because services is down : ", ex.getMessage());
+
+        User user = new User();
+        user.setEmail("dummy@mail.com");
+        user.setName("dummy user");
+        user.setUserId("dummy rating");
+        user.setAbout("this user create dummy because some service is down :");
+        return new ResponseEntity<>(user,HttpStatus.BAD_REQUEST);
+    }
+
 
     @GetMapping("/all")
     public ResponseEntity<List<User>> getAllUsersHandler(){
