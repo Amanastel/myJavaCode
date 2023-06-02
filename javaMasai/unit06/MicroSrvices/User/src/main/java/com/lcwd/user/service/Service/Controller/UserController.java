155fb5c8371ae97ad8e419380ef2a60eaacb6d1c
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-02 05:38:27 +0530

    rateLimiter

--- javaMasai/unit06/MicroSrvices/User/src/main/java/com/lcwd/user/service/Service/Controller/UserController.java
+++ javaMasai/unit06/MicroSrvices/User/src/main/java/com/lcwd/user/service/Service/Controller/UserController.java
@@ -4,6 +4,7 @@ package com.lcwd.user.service.Service.Controller;
 import com.lcwd.user.service.Service.entities.User;
 import com.lcwd.user.service.Service.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,8 @@ public class UserController {
     int retryCount = 0;
     @GetMapping("/{userId}")
 //    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
-    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+//    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserByIdHandler(@PathVariable String userId){
         log.info("Retry count {}: ",retryCount);
         retryCount++;
