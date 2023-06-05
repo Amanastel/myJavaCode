6a5998e6de6400b1995822c97e23f2f2d1ab6caf
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-05 05:32:25 +0530

    oder service

---
+++ javaMasai/unit06/micro-service2/oders/src/main/java/com/microservice2/os/api/OderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice2.os.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OderServiceApplication.class, args);
+	}
+
+}
---
+++ javaMasai/unit06/micro-service2/oders/src/test/java/com/microservice2/os/api/OderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice2.os.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
