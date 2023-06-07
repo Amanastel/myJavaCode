ab90a4d9863e896249b2ddd436127af040e5dd1f
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-08 03:29:13 +0530

    sprint security

---
+++ javaMasai/unit06/spring-security-b26/sprint-4b26/class1/src/main/java/com/masai/SpringSecurityClassApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClassApplication.class, args);
+	}
+
+}
