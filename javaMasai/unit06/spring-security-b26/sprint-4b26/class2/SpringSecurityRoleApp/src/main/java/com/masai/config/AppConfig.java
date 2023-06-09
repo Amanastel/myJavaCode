b89557a3fe78c173f2b1f00b6ea220e10d313573
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-09 20:13:20 +0530

    role base

---
+++ javaMasai/unit06/spring-security-b26/sprint-4b26/class2/SpringSecurityRoleApp/src/main/java/com/masai/config/AppConfig.java
@@ -0,0 +1,45 @@
+package com.masai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+	
+
+	@Bean
+	public SecurityFilterChain springSecurityConfiguration(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(auth ->{
+			
+			auth
+			.requestMatchers(HttpMethod.POST,"/customers").permitAll()
+		.requestMatchers(HttpMethod.GET, "/customers","/hello").hasRole("ADMIN")
+		.requestMatchers(HttpMethod.GET, "/customers/**").hasAnyRole("ADMIN","USER")
+			.anyRequest().authenticated();
+			
+		})
+		.csrf(csrf -> csrf.disable())
+		.formLogin(Customizer.withDefaults())
+		.httpBasic(Customizer.withDefaults());
+		
+		
+		return http.build();
+
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
+
+	}
+	
+
+}
